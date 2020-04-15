f221469f162b58ef8d38260b24743f0234f99b8a
mmashop-creator <63697263+mmashop-creator@users.noreply.github.com>
2020-04-15 12:12:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+
+	$sub1 = $_GET[st_sub1];
+	$sub2 = $_GET[st_sub2];
+	
+	$link = 'https://hskwq.com/click-HQHHAFZM-KGCQCJ47?bt=25&tl=1&sa=[SUBID]&st_sub1='.$sub1.'&st_sub2='.$sub2; // ссылка с проклы на ленд
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Рады приветствовать!</title>
+	<link rel="stylesheet" href="css/styles.css">
+
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+	<script type="text/javascript" src="//topsalezz.org/js/prelend-utils.js"></script>
+
+	<script type="text/javascript"> 
+		var default_rulr = "https://hskwq.com/click-HQHHAFZM-KGCQCJ47?bt=25&tl=1&sa=et1"; 
+		if (typeof PrelendUtils == "undefined") { 
+		function go_away () { 
+		location.href = default_rulr; 
+		} 
+		} else { 
+		new PrelendUtils({ 
+		loadAudioURL: "audio/1.ogg", 
+		unloadAudioURL: "audio/2.ogg", 
+		cmbrMessage: "**********************************\n\nВНИМАНИЕ!!! УНИКАЛЬНЫЙ ШАНС! СКИДКА 50%!\n\n**********************************\n\nТолько СЕЙЧАС в течение 30 минут у Вас есть шанс получить скидку 50%!\n\nНажмите на кнопку \"Остаться на странице\" и получите ГРАНДИОЗНУЮ СКИДКУ!\n\n Осталось 9 упаковок по спец. цене!" //текст камбекера, отличный от дефолта 
+		
+		}); 
+		} 
+	</script> 
+
+
+</head>
+
+<body>
+	
+	<section>
+		<div class="dialogue">
+			<h1 class="dialogue_title">
+				Прежде чем Вы войдете в Игру #1 с рейтингом <br>
+				18+, мы должны задать вам несколько <br> вопросов.
+			</h1>
+			<div class="dialogue_questions">
+				<div class="question question-1">
+					<p>1. Какой пол персонажа вам предпочтительнее?</p>
+					<div class="question_icons">
+						<div class="question_icon question-1__btn icon__man">
+							<span>Мужcкой</span>
+						</div>
+						<div class="question_icon question-1__btn icon__woman">
+							<span>Женский</span>
+						</div>
+					</div>
+				</div>
+				<div class="question question-2">
+					<p>2. Ваш любимый класс в ролевых играх?</p>
+					<div class="question_icons">
+						<div class="question_icon question-2__btn icon__warrior">
+							<span>Воин</span>
+						</div>
+						<div class="question_icon question-2__btn icon__wizard">
+							<span>Маг</span>
+						</div>
+						<div class="question_icon question-2__btn icon__archer">
+							<span>Лучник</span>
+						</div>
+					</div>
+				</div>
+				<div class="question question-3">
+					<p>3. Какая расса героя для вас предпочтительнее?</p>
+					<div class="question_icons">
+						<div class="question_icon question-3__btn icon__people">
+							<span>Человек</span>
+						</div>
+						<div class="question_icon question-3__btn icon__elf">
+							<span>Эльф</span>
+						</div>
+						<div class="question_icon question-3__btn icon__dwarf">
+							<span>Гном</span>
+						</div>
+					</div>
+				</div>
+				<div class="question continue">
+					<p>Ваш персонаж сгенерирован, для продолжения <br> нажмите кнопку</p>
+					<div class="question_icons">
+						<a href="#" class="btn_continue"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<script type="text/javascript" src="js/script.js"></script>
+
+	<script type="text/javascript"> 
+		document.addEventListener("DOMContentLoaded", function(){ 
+		
+		var anchors = document.getElementsByTagName("a"); 
+		
+		for (var i = 0; i < anchors.length; i++) { 
+		anchors[i].href = "javascript:void(0)"; 
+		anchors[i].target = "_self"; 
+		anchors[i].setAttribute("onclick", "go_away();return false;"); 
+		anchors[i].setAttribute("oncontextmenu", "return false;"); 
+		} 
+		}); 
+	</script>
+</body>
+
+</html>
